b6d8d5a1169335aabfdaa1cb209ea292ea7bfabd
JoaoRiuto <jr00.dat@gmail.com>
2020-01-02 12:26:07 -0300

    Nova classe MainMap para testes de inserção Chave / Valor

--- src/br/com/treinaweb/java/collections/MainHashTable.java
+++ src/br/com/treinaweb/java/collections/MainHashTable.java
@@ -1,6 +1,8 @@
 package br.com.treinaweb.java.collections;
 
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Iterator;
 
 import br.com.treinaweb.java.collections.models.Pessoa;
 
@@ -16,6 +18,51 @@ public class MainHashTable {
 		System.out.println(tabelaPessoas);
 		
 		tabelaPessoas.get("legal");
+		
+		//Iteração
+		//Fail-fast iterator
+//		Iterator<String> iteratorChaves = tabelaPessoas.keySet().iterator();
+//		//tabelaPessoas.remove("legal");
+//		while (iteratorChaves.hasNext()) {
+//			String chave = iteratorChaves.next();
+//			System.out.println(" - " + tabelaPessoas.get(chave).toString());
+//		}
+		
+		//Non Fail-fast iterator
+		
+		Enumeration<String> chaves = tabelaPessoas.keys();
+		tabelaPessoas.remove("legal");
+		while (chaves.hasMoreElements()) {
+			String chave = chaves.nextElement();
+			System.out.println(" - " + tabelaPessoas.get(chave).toString());
+		}
+		
+		//Verificador antigo
+//		Pessoa p = null;
+//		if (tabelaPessoas.containsKey("legal")) {
+//			p = tabelaPessoas.get("legal");
+//		} else {
+//			p = new Pessoa(-1, "Não existe");
+//		}
+//		System.out.println(p);
+		
+		//Novo jeito orientado à funções
+		
+		Pessoa p = tabelaPessoas.getOrDefault("legal",new Pessoa(-1, "Non EqXiste"));
+		System.out.println(p);
+		
+		
+//		if (!tabelaPessoas.containsKey("legal")) {
+//			tabelaPessoas.put("legal", new Pessoa (1, "TreinaWeb"));
+//		}
+		
+		tabelaPessoas.putIfAbsent("legal", new Pessoa(1, "TreinaWeb 1"));
+		System.out.println(tabelaPessoas);
+		
+		tabelaPessoas.forEach((chave, valor) -> {
+			System.out.println(String.format("[%s] %s", chave, valor.toString()));
+		});
+		
 
 	}
 
